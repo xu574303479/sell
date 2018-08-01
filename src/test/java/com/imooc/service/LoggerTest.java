2ae266a8a826872a8236ea1cb814cfd20064b270
xu574303479 <574303479@qq.com>
2018-08-01 23:59:06 +0800

    test commit

---
+++ src/test/java/com/imooc/service/LoggerTest.java
@@ -0,0 +1,29 @@
+package com.imooc.service;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @Author xuhaibin
+ * @ClassName: LoggerTest
+ * @Description:日志测试类
+ * @Version 1.0.0
+ * @Date 2018/8/1 23:53
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggerTest {
+
+    private final static Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+
+
+
+
+
+
+}
